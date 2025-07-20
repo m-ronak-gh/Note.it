b196e108dd1dee025f080f3a68b7ed653aad9a77
m-ronak-gh <181626524+m-ronak-gh@users.noreply.github.com>
2025-07-20 09:43:15 +0000

    Prepared for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path"
 import cors from "cors";
 
 import notesRoutes from './routes/notesRoutes.js';
@@ -10,11 +11,14 @@ import ratelimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-app.use(cors({
-    origin:"http://localhost:5173",
-}));
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin:"http://localhost:5173",
+    }));
+}
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(ratelimiter);
 
@@ -26,6 +30,15 @@ app.use(ratelimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    });    
+};
+
 connectDB().then(() => { //connect database and then start the server/app
     app.listen(PORT, () => {
         console.log("Server started on PORT: ",PORT);
